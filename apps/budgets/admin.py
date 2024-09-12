889310ad7a8def1a4fdd156539f6c020c49a6583
Felipe <lipe.h.r@gmail.com>
2024-09-11 22:57:32 -0300

    :tada: initial commit

---
+++ apps/budgets/admin.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+from io import BytesIO
+
+from django.contrib import admin
+from django.core.files.base import ContentFile
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+
+from .models import Budget, Client, Material
+
+
+class MaterialInline(admin.TabularInline):
+    model = Material
+    extra = 1
+    readonly_fields = ["total_price"]
+    fields = ["description", "quantity", "unit_price", "total_price"]
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("name", "document", "address")
+    search_fields = ("name", "document", "address")
+
+
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ("client", "budget_date", "total_amount")
+    inlines = [MaterialInline]
+    search_fields = ("client__name", "service_description")
+    actions = ["generate_pdf_action"]
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+
+    def save_related(self, request, form, formsets, change):
+        super().save_related(request, form, formsets, change)
+        form.instance.recalculate_totals()
+
+    @admin.action(description="Generate PDF for selected budgets")
+    def generate_pdf_action(self, request, queryset):
+        for budget in queryset:
+            self.generate_pdf(budget)
+        self.message_user(
+            request, "PDFs generated successfully for the selected budgets."
+        )
+
+    def generate_pdf(self, budget):
+        template = get_template("budgets/budget_pdf.html")
+        html = template.render({"budget": budget})
+
+        result = BytesIO()
+        pdf = pisa.CreatePDF(BytesIO(html.encode("utf-8")), dest=result)
+
+        if not pdf.err:
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            client_name = budget.client.name.replace(" ", "_")
+            budget.pdf_file.save(
+                f"ORCAMENTO_{budget.id}_{client_name}_{timestamp}.pdf",
+                ContentFile(result.getvalue()),
+            )
+            result.close()
+
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Budget, BudgetAdmin)
---
+++ apps/budgets/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    document = models.CharField(max_length=18, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Budget(models.Model):
+    client = models.ForeignKey(Client, related_name="budgets", on_delete=models.CASCADE)
+    budget_date = models.DateField()
+    validity_days = models.PositiveIntegerField(default=30)
+
+    service_description = models.TextField()
+
+    workforce = models.FloatField()
+    total_materials = models.FloatField(blank=True, null=True)
+    total_amount = models.FloatField(blank=True, null=True)
+
+    notes = models.TextField(blank=True, null=True)
+
+    pdf_file = models.FileField(upload_to="budgets/", blank=True, null=True)
+
+    def recalculate_totals(self):
+        self.total_materials = sum(
+            material.total_price for material in self.materials.all()
+        )
+        self.total_amount = self.total_materials + self.workforce
+
+        self.save()
+
+    def __str__(self):
+        return f"Budget {self.id} for {self.client.name} - {self.budget_date}"
+
+
+class Material(models.Model):
+    budget = models.ForeignKey(
+        Budget, related_name="materials", on_delete=models.CASCADE
+    )
+    description = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.FloatField()
+    total_price = models.FloatField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.quantity * self.unit_price
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.description} - {self.total_price}"
